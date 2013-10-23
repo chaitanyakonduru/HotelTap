d677a1264b3e84ecca038ac05f4a9b0f3b4eeabd
chaitanya <chaitu.cn@gmail.com>
2013-10-23 15:55:14 +0530

    Programatic Commits

--- src/com/example/restaurant/MainActivity.java
+++ src/com/example/restaurant/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.restaurant;
 
+import com.example.hoteltaprestaurant.R;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
@@ -12,14 +14,13 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
 		Log.v(TAG, "Test");
+		setContentView(R.layout.layout_main);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
 
